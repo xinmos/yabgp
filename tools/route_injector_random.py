d012a6cfd34cd33d4060345a2a9bbeff5d5e44e4
xinqxion <xinqxion@cisco.com>
2022-06-08 14:13:10 +0800

    feat: support address family--evpn: type 2, ipv4/ipv6:unicast, label unicast and mpls vpn

--- tools/route_injector_random.py
+++ tools/route_injector_random.py
@@ -17,10 +17,12 @@
 #    under the License.
 
 """ BGP Route injector
+    cmd: python route_injector_random.py --rest-host={ip} --rest-port={port} --count={count} --afi-safi={afi_safi}
 """
 
 from __future__ import print_function
 
+import ipaddress
 import random
 import sys
 import urllib.request
@@ -35,7 +37,12 @@ CONF = cfg.CONF
 CONF.register_cli_opts([
     cfg.StrOpt('peerip', help='The BGP peer address'),
     cfg.IntOpt('interval', default=0, help='time interval when sending message'),
-    cfg.IntOpt('count', default=10000, help='the number of messages you want to generate')]
+    cfg.IntOpt('count', default=10000, help='the number of messages you want to generate'),
+    cfg.StrOpt('afi-safi',
+               default='ipv4_unicast',
+               help='the address family of messages you want to generate',
+               choices=['ipv4_unicast', 'ipv4_mpls_vpn', 'ipv4_label_unicast', 'ipv6_unicast', 'ipv6_mpls_vpn',
+                        'ipv6_label_unicast', 'evpn'])]
 )
 
 rest_server_ops = [
@@ -124,9 +131,8 @@ def get_data_from_agent(url, username, password, method='GET', data=None):
     opener_v1 = get_api_opener_v1(url, username, password)
     try:
         res = json.loads(opener_v1.open(request).read())
-        return True
+        return res['status']
     except Exception as e:
-        print(e)
         return False
 
 
@@ -134,55 +140,257 @@ def interval():
     time.sleep(CONF.interval)
 
 
-def get_random_ipv4_addr():
-    mask_randmon = random.randint(16, 32)
-    ip_addr = f"{random.getrandbits(8)}.{random.getrandbits(8)}.{random.getrandbits(8)}.{random.getrandbits(8)}"
-    return ip_addr + "/" + str(mask_randmon)
+def get_random_ip(v):
+    """
+    :param v: 4: ipv4, 6: ipv6
+    """
+    if v == 4:
+        ipv4 = ipaddress.IPv4Address(random.getrandbits(32))
+        return str(ipv4)
+    elif v == 6:
+        ipv6 = ipaddress.IPv6Address(random.getrandbits(128))
+        # .compressed contains the short version of the IPv6 address
+        # str(ipv6) always returns the short address
+        # .exploded is the opposite of this, always returning the full address with all-zero groups and so on
+        return str(ipv6.compressed)
+
+
+def get_random_ip_mask(v):
+    """
+    :param v: 4: ipv4, 6: ipv6
+    """
+    if v == 4:
+        return random.randint(16, 32)
+    elif v == 6:
+        return random.randint(64, 128)
+
+
+def get_random_list(length=10, random_type=1):
+    """
+    :param random_type: 1:list, 2:community, 3:ip addr
+    """
+    tmp_len = random.randint(1, length)
+    tmp_set = set()
+    if random_type == 1:
+        for _ in range(tmp_len):
+            tmp_set.add(random.getrandbits(16))
+    elif random_type == 2:
+        for _ in range(tmp_len):
+            tmp_set.add(f"{random.getrandbits(16)}:{random.getrandbits(16)}")
+    elif random_type == 3:
+        for _ in range(tmp_len):
+            tmp_set.add(f"{get_random_ip(4)}/{get_random_ip_mask(4)}")
+
+    return list(tmp_set)
 
 
 def init_ipv4_unicast_template():
-    # list_random length < 10
-    random_len = random.randint(1, 10)
-    random_set = set()
-    for _ in range(random_len):
-        random_set.add(random.getrandbits(16))
-
-    # community length < 10
-    community_len = random.randint(1, 10)
-    community_set = set()
-    for _ in range(community_len):
-        community_set.add(f"{random.getrandbits(16)}:{random.getrandbits(16)}")
-
+    random_list = get_random_list(10, 1)
+    community_list = get_random_list(10, 2)
     color_random = random.getrandbits(16)
-
-    # nlri length < 20
-    nlri_len = random.randint(1, 20)
-    nlri_random = set()
-    for _ in range(nlri_len):
-        nlri_random.add(get_random_ipv4_addr())
+    nlri_random_list = get_random_list(20, 3)
 
     message_json = {
-        "type": 2,
-        "msg": {
-            "attr": {
-                "1": 0,
-                "2": [[2, list(random_set)]],
-                "3": "192.0.2.1",
-                "5": 500,
-                "8": list(community_set),
-                "16": [f"color:{color_random}"]
+        "attr": {
+            "1": 0,
+            "2": [[2, random_list]],
+            "3": "192.0.2.1",
+            "5": 500,
+            "8": community_list,
+            "16": [f"color:{color_random}"]
+        },
+        "nlri": nlri_random_list
+    }
+
+    return message_json
+
+
+def init_ipv4_mpls_vpn_template():
+    label_list = get_random_list(5, 1)
+    rd = f"{random.getrandbits(16)}:{random.getrandbits(16)}"
+    color_random = random.getrandbits(16)
+    nlri_list_len = 3
+    nlri_list = []
+    for _ in range(nlri_list_len):
+        nlri_list.append({
+            "label": label_list,
+            "rd": f"{random.getrandbits(16)}:{random.getrandbits(16)}",
+            "prefix": f"{get_random_ip(4)}/{get_random_ip_mask(4)}"
+        })
+
+    message_json = {
+        "attr": {
+            "1": 0,
+            "2": [],
+            "5": 100,
+            "14": {
+                "afi_safi": [1, 128],
+                "nexthop": {"rd": rd, "str": get_random_ip(4)},
+                "nlri": nlri_list
             },
-            "nlri": list(nlri_random)
+            "16": [f"route-target:{random.getrandbits(16)}:{random.getrandbits(16)}", f"color:{color_random}"]
         }
     }
 
     return message_json
 
 
+def init_ipv4_label_unicast_template():
+    color_random = random.getrandbits(16)
+    label_list = get_random_list(5, 1)
+    nlri_list_len = 10
+    nlri_list = []
+    for _ in range(nlri_list_len):
+        nlri_list.append({
+            "label": label_list,
+            "prefix": f"{get_random_ip(4)}/{get_random_ip_mask(4)}"
+        })
+    message_json = {
+        "attr": {
+            "1": 0,
+            "2": [],
+            "5": 400,
+            "14": {
+                "afi_safi": [1, 4],
+                "nexthop": get_random_ip(4),
+                "nlri": nlri_list
+            },
+            "16": [f"color:{color_random}"]
+        }
+    }
+
+    return message_json
+
+
+def init_ipv6_unicast_template():
+    random_list = get_random_list(10, 1)
+    community_list = get_random_list(10, 2)
+    nlri_list_len = 3
+    nlri_list = []
+    for _ in range(nlri_list_len):
+        nlri_list.append(f"{get_random_ip(6)}/{get_random_ip_mask(6)}")
+    message_json = {
+        "attr": {
+            "1": 0,
+            "2": [[2, random_list]],
+            "5": 500,
+            "8": community_list,
+            "14": {
+                "afi_safi": [2, 1],
+                "nexthop": get_random_ip(6),
+                "nlri": nlri_list
+            }
+        },
+        "nlri": []
+    }
+
+    return message_json
+
+
+def init_ipv6_mpls_vpn_template():
+    rd = f"{random.getrandbits(16)}:{random.getrandbits(16)}"
+    color_random = random.getrandbits(16)
+    label_list = get_random_list(5, 1)
+    nlri_list_len = 3
+    nlri_list = []
+    for _ in range(nlri_list_len):
+        nlri_list.append({
+            "label": label_list,
+            "rd": f"{random.getrandbits(16)}:{random.getrandbits(16)}",
+            "prefix": f"{get_random_ip(6)}/{get_random_ip_mask(6)}"
+        })
+
+    message_json = {
+        "attr": {
+            "1": 0,
+            "2": [],
+            "5": 100,
+            "14": {
+                "afi_safi": [2, 128],
+                "nexthop": {"rd": rd, "str": get_random_ip(6)},
+                "nlri": nlri_list
+            },
+            "16": [f"route-target:{random.getrandbits(16)}:{random.getrandbits(16)}", f"color:{color_random}"]
+        }
+    }
+
+    return message_json
+
+
+def init_ipv6_label_unicast_template():
+    label_list = get_random_list(5, 1)
+    nlri_list_len = 3
+    nlri_list = []
+    for _ in range(nlri_list_len):
+        nlri_list.append({
+            "label": label_list,
+            "prefix": f"{get_random_ip(6)}/{get_random_ip_mask(6)}"
+        })
+    message_json = {
+        "attr": {
+            "1": 0,
+            "2": [],
+            "5": 400,
+            "14": {
+                "afi_safi": [2, 4],
+                "nexthop": get_random_ip(6),
+                "nlri": nlri_list
+            }
+        }
+    }
+
+    return message_json
+
+
+def init_evpn_template():
+    rd = f"{get_random_ip(4)}:{random.getrandbits(16)}"
+    mac_list = [0x58, 0x96, 0x1D, random.getrandbits(8), random.getrandbits(8), random.getrandbits(8)]
+    mac = '-'.join(map(lambda x: "%02x" % x, mac_list))
+    message_json = {
+        "attr": {
+            "1": 0,
+            "2": [],
+            "5": 100,
+            "14": {
+                "afi_safi": [25, 70],
+                "nexthop": get_random_ip(4),
+                "nlri": [
+                    {
+                        "type": 2,
+                        "value": {
+                            "eth_tag_id": 108,
+                            "ip": get_random_ip(4),
+                            "label": [0],
+                            "rd": rd,
+                            "mac": mac,
+                            "esi": 0
+                        }
+                    }
+                ]
+            },
+            "16": ["mac-mobility:1:500"]
+        }
+    }
+
+    return message_json
+
+
+MSG_TYPE_DICT = {
+    'ipv4_unicast': init_ipv4_unicast_template,
+    'ipv4_mpls_vpn': init_ipv4_mpls_vpn_template,
+    'ipv4_label_unicast': init_ipv4_label_unicast_template,
+    'ipv6_unicast': init_ipv6_unicast_template,
+    'ipv6_mpls_vpn': init_ipv6_mpls_vpn_template,
+    'ipv6_label_unicast': init_ipv6_label_unicast_template,
+    'evpn': init_evpn_template,
+}
+
+
 def send_update():
     url = 'http://{bind_host}:{bind_port}/v1/peer/{peer_ip}/send/update'
     url = url.format(bind_host=CONF.rest.host, bind_port=CONF.rest.port, peer_ip=CONF.peerip)
     message_count = CONF.count
+    message_type = CONF.afi_safi
     bar_length = 50
     message_pass_send = 0
     send_success = 0
@@ -191,43 +399,21 @@ def send_update():
     percent_step = 0.01
 
     for _ in range(message_count):
-        message_json = init_ipv4_unicast_template()
-        if message_json['type'] != 2:
-            message_count -= 1
-            continue
-        message = message_json['msg']
-        if message['nlri']:
-            if CONF.attribute.no_origin_cluster:
-                if message['attr'].get('9'):
-                    message['attr'].pop('9')
-                if message['attr'].get('10'):
-                    message['attr'].pop('10')
-            elif CONF.attribute.originator_id and CONF.attribute.cluster_list:
-                message['attr']['9'] = CONF.attribute.originator_id
-                message['attr']['10'] = CONF.attribute.cluster_list
-            if CONF.attribute.nexthop:
-                message['attr']['3'] = CONF.attribute.nexthop
-            post_data = {
-                'nlri': message['nlri'],
-                'attr': message['attr']
-            }
-            res = get_data_from_agent(url, 'admin', 'admin', 'POST', post_data)
-            if res:
-                send_success += 1
-                interval()
-            else:
-                send_failed += 1
-            while message_pass_send / message_count >= current_percent / 100:
-                hashes = '#' * int(message_pass_send / message_count * bar_length)
-                spaces = ' ' * (bar_length - len(hashes))
-                sys.stdout.write("\rPercent: [%s] %.2f%%" % (hashes + spaces, current_percent))
-                sys.stdout.flush()
-                current_percent += percent_step
-            message_pass_send += 1
+        message = MSG_TYPE_DICT[message_type]()
+        res = get_data_from_agent(url, 'admin', 'admin', 'POST', message)
+        if res:
+            send_success += 1
+            interval()
         else:
-            # TODO support other address family
-            message_count -= 1
-            continue
+            send_failed += 1
+        while message_pass_send / message_count >= current_percent / 100:
+            hashes = '#' * int(message_pass_send / message_count * bar_length)
+            spaces = ' ' * (bar_length - len(hashes))
+            sys.stdout.write("\rPercent: [%s] %.2f%%" % (hashes + spaces, current_percent))
+            sys.stdout.flush()
+            current_percent += percent_step
+        message_pass_send += 1
+
     hashes = '#' * bar_length
     spaces = ''
     sys.stdout.write("\rPercent: [%s] %.2f%%" % (hashes + spaces, 100.00))
